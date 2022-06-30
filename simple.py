d7b0e7eb82ab9e1163a442d3d99e93e5cd9ae187
Yuriyji <108481556+Yuriyji@users.noreply.github.com>
2022-06-30 13:05:17 -0400

    Add files via upload
    This program asks your name and your favorite color. Then calculates your age off of the given birth year.

---
+++ simple.py
@@ -0,0 +1,8 @@
+from ast import Return
+name = input("what is your name?")
+favorite_color= input('what is your favorite color?')
+print(name+ ' likes ' + favorite_color)
+birth_year = input('What year were you born in?')
+age = 2022-int(birth_year)
+print(age)
+input('press any key to exit ;')
