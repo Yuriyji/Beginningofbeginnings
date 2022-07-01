f5957b6906b233545b635d18fffd34d50fd6f896
Yuriyji <108481556+Yuriyji@users.noreply.github.com>
2022-07-01 10:59:51 -0400

    Add files via upload
    Learned about breaking up strings and copying lines by [0:] brackets

---
+++ Day2.py
@@ -0,0 +1,10 @@
+#course = 'Cant touch me'
+#print(course [0:4])
+#another = course[:]
+#print(another)
+#{} <-CREATES HOLES IN THE STRING}
+first= 'yuriy'
+last= 'grishchuk'
+msg = f'{first} {last} is a coder' 
+#f calls a function
+print(msg)
